ca08c432fe14df72a32fafc0fd73c241243883a7
iamishuchawla <chawlaishu1@gmail.com>
2021-09-05 19:54:18 +0530

    Updates

---
+++ precache-manifest.1e6005f835a2b9f59375a5014efd3c4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d526c0dfca871f817271eb58eabf75",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "91f66d2baf8613e7b87f",
+    "url": "/react-tic-tac-toe/static/css/main.c9656128.chunk.css"
+  },
+  {
+    "revision": "e1c6a816ccf14fa6bf7d",
+    "url": "/react-tic-tac-toe/static/js/2.9ead5ae3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-tic-tac-toe/static/js/2.9ead5ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f66d2baf8613e7b87f",
+    "url": "/react-tic-tac-toe/static/js/main.00188562.chunk.js"
+  },
+  {
+    "revision": "d9255e6de55285e18dc3",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.81654cbd.js"
+  }
+]);
